c73ef08a48b3b0f213e21c94d39d514b5ccf8714
Aayushi-Uniyal <auniyal_be24@thapar.edu>
2025-10-26 19:57:33 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -1,34 +1,37 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-struct Node {
-    int data;
-    Node* next;
-};
+struct Node { int data; Node* next; };
 Node* head = NULL;
 
-void insertEnd(int val) {
-    Node* t = new Node{val, NULL};
-    if (!head) {
-        head = t;
-        t->next = head;
-    } else {
+void insertEnd(int x){
+    Node* t = new Node{x, NULL};
+    if(!head){ head = t; t->next = head; }
+    else {
         Node* p = head;
-        while (p->next != head) p = p->next;
+        while(p->next != head) p = p->next;
         p->next = t;
         t->next = head;
     }
 }
 
-void display() {
+void display(){
     Node* p = head;
-    if (!head) return;
+    if(!p) return;
     do {
         cout << p->data << " ";
         p = p->next;
-    } while (p != head);
-    cout << head->data << endl; // repeat head at end
+    } while(p != head);
+    cout << head->data; // repeat head
 }
 
-int main() {
-    int arr[] = {20,100
+int main(){
+    int n, x;
+    cout << "Enter number of nodes: ";
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> x;
+        insertEnd(x);
+    }
+    display();
+}
